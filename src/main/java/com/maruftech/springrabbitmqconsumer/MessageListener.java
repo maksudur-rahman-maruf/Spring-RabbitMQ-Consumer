1f4f09e221edb96029929339cf7d2dd0cb72c174
Maksudur Rahman Maruf <43263023+maksudur-rahman-maruf@users.noreply.github.com>
2021-12-01 19:26:30 +0600

    Add files via upload

---
+++ src/main/java/com/maruftech/springrabbitmqconsumer/MessageListener.java
@@ -0,0 +1,15 @@
+package com.maruftech.springrabbitmqconsumer;
+
+import com.rabbitmq.client.Channel;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageListener {
+
+    @RabbitListener(queues = MQConfig.QUEUE)
+    public void listener(CustomMessage message) {
+        System.out.println(message);
+    }
+
+}
---
+++ src/test/java/com/maruftech/springrabbitmqconsumer/SpringRabbitmqConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maruftech.springrabbitmqconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRabbitmqConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
